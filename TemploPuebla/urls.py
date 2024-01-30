489937883c5d39646e341c82c59a6748eeee49b2
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2024-01-30 18:23:05 +0100

    final commit

--- TemploPuebla/urls.py
+++ TemploPuebla/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index, tecnologia, seguridad, estacionamiento, recorridos, traduccion, facilidades
 
 urlpatterns = [
     path('', index, name='index'),
+    path('tecnologia/', tecnologia, name='tecnologia'),
+    path('seguridad/', seguridad, name='seguridad'),
+    path('estacionamiento/', estacionamiento, name='estacionamiento'),
+    path('recorridos/', recorridos, name='recorridos'),
+    path('traduccion/', traduccion, name='traduccion'),
+    path('facilidades/', facilidades, name='facilidades'),
     path('admin/', admin.site.urls),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def tecnologia(request):
+    return render(request, 'core/tecnologia.html')
+
+def seguridad(request):
+    return render(request, 'core/seguridad.html')
+
+def estacionamiento(request):
+    return render(request, 'core/estacionamiento.html')
+
+def recorridos(request):
+    return render(request, 'core/recorridos.html')
+
+def traduccion(request):
+    return render(request, 'core/traduccion.html')
+
+def facilidades(request):
+    return render(request, 'core/facilidades.html')
